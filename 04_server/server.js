2184afd2010b06c78222fe5c7cda87e933a8a1c5
yoo16 <yohei.yoshikawa@gmail.com>
2021-10-31 12:26:20 +0900

    UD

--- 04_server/server.js
+++ 04_server/server.js
@@ -2,16 +2,13 @@
 
 // http モジュールを読み込み
 const http = require('http');
-//const httpStatus = require('http-status-codes');
-
 const port = 3000;
-const host = '127.0.0.1';
+const host = 'localhost';
 const message = "<h1>Hello Node Server!</h1>\n";
 
 const app = http.createServer((request, response) => {
     // HTTPヘッダ
     response.writeHead(200, {'Content-Type': 'text/html'});
-    //response.writeHead(httpStatus.OK, {'Content-Type': 'text/html'});
 
     // レスポンス書き込み
     response.write(message);
@@ -26,4 +23,4 @@ const app = http.createServer((request, response) => {
 // ホストとポートを指定して監視
 app.listen(port, host);
 
-console.log(`Server listen: ${host}:${port}`);
+console.log(`Server listen: http://${host}:${port}`);
