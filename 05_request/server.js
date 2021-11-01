80dc72c1a7c013124b9a12ef3f1af571e480a568
yoo16 <yohei.yoshikawa@gmail.com>
2021-11-01 18:14:45 +0900

    UC

--- 05_request/server.js
+++ 05_request/server.js
@@ -1,22 +1,18 @@
 "use strict";
 
 const http = require('http');
-const httpStatus = require('http-status-codes');
 const querystring = require('querystring');
 const fs = require('fs');
-const config = require('config');
-require('dotenv').config()
-
-
 const html = fs.readFileSync('index.html');
-// const port = config.server.port;
-// const host = config.server.host;
-const host = process.env.HOST
+
+require('dotenv').config();
+const host = process.env.HOST;
 const port = process.env.PORT;
 
-const app = http.createServer(function (request, response) {
+const app = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    
     let post = '';
-
     request.on('data', (value) => {
         post+= value;
     });
@@ -27,7 +23,6 @@ const app = http.createServer(function (request, response) {
         }
         response.end(html);
     });
-    response.writeHead(httpStatus.OK, {'Content-Type': 'text/html'});
 
     console.log(`Method: ${request.method}`);
     console.log(`URL: ${request.url}`);
